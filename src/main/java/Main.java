b29cbf0ed4154f4d7d25d3c36d045b7f43b20983
Luke Bermingham <luke.bermingham@2bitstudios.com.au>
2017-03-18 22:47:34 +1000

    Updated locators, still tweaking for accuracy.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,15 @@
+import onethreeseven.ydlbot.model.DuelistLocator;
+
 /**
  * Entry point for the bot.
  * @author Luke Bermingham
  */
 public class Main {
+
+    public static void main(String[] args) {
+        nu.pattern.OpenCV.loadShared();
+        new DuelistLocator().findDuelist();
+
+    }
+
 }
--- src/main/java/onethreeseven/ydlbot/model/DuelistLocator.java
+++ src/main/java/onethreeseven/ydlbot/model/DuelistLocator.java
@@ -6,9 +6,10 @@ import org.opencv.features2d.DescriptorExtractor;
 import org.opencv.features2d.DescriptorMatcher;
 import org.opencv.features2d.FeatureDetector;
 import org.opencv.imgcodecs.Imgcodecs;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.opencv.core.CvType.CV_32F;
 
 
 /**
@@ -31,21 +32,32 @@ public class DuelistLocator {
 //        System.out.println(String.format("Detected %s faces", detections.toArray().length));
 //    }
 
-    private static final FeatureDetector featureDetector = FeatureDetector.create(FeatureDetector.SURF);
-    private static final DescriptorExtractor descriptorExtractor = DescriptorExtractor.create(DescriptorExtractor.SURF);
-    private static final Mat duelistImg = Imgcodecs.imread(DuelistLocator.class.getResource("/lena.png").getPath());
-    private static final DescriptorMatcher descriptorMatcher = DescriptorMatcher.create(DescriptorMatcher.FLANNBASED);
-    private static final float nndrRatio = 0.7f;
+    private static final FeatureDetector featureDetector = FeatureDetector.create(FeatureDetector.ORB);
+    private static final DescriptorExtractor descriptorExtractor = DescriptorExtractor.create(DescriptorExtractor.ORB);
+    private static final DescriptorMatcher descriptorMatcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE);
+    private static final float nndrRatio = 0.85f;
 
-    private final Mat duelistDescriptor;
+    private final Mat imageToLookFor;
+    private final Mat imageDescriptor;
+    private final MatOfKeyPoint imageKeyPoints;
+
+    //private final Mat duelistDescriptor;
 
     public DuelistLocator(){
-        this.duelistDescriptor = getDescriptors(duelistImg);
+        //String imgPath = Thread.currentThread().getContextClassLoader().getResource("save.png").getPath();
+        String imgPath = "C:\\Users\\luke\\Desktop\\projects\\ydl-bot\\src\\main\\resources\\key3.png";
+        imageToLookFor = Imgcodecs.imread(imgPath, Imgcodecs.IMREAD_COLOR);
+        imageKeyPoints = getKeyPoints(imageToLookFor);
+        imageDescriptor = getDescriptors(imageToLookFor, imageKeyPoints);
     }
 
-    private Mat getDescriptors(Mat image){
+    private MatOfKeyPoint getKeyPoints(Mat image){
         MatOfKeyPoint objectKeyPoints = new MatOfKeyPoint();
         featureDetector.detect(image, objectKeyPoints);
+        return objectKeyPoints;
+    }
+
+    private Mat getDescriptors(Mat image, MatOfKeyPoint objectKeyPoints){
         Mat descriptor = new Mat();
         descriptorExtractor.compute(image, objectKeyPoints, descriptor);
         return descriptor;
@@ -54,21 +66,63 @@ public class DuelistLocator {
     public void findDuelist(){
         //get the duel links scene and descriptors
         Mat frame = ImageUtil.getOpenCVScreenShot();
-        Mat frameDescriptors = getDescriptors(frame);
+        MatOfKeyPoint frameKeyPoints = getKeyPoints(frame);
+        Mat frameDescriptors = getDescriptors(frame, frameKeyPoints);
         //find matches of the duelist image in the scene
         List<MatOfDMatch> matches = new ArrayList<>();
-        descriptorMatcher.knnMatch(duelistDescriptor, frameDescriptors, matches, 2);
-        pruneMatches(matches);
+
+        imageDescriptor.convertTo(imageDescriptor, CV_32F);
+        frameDescriptors.convertTo(frameDescriptors, CV_32F);
+
+        descriptorMatcher.knnMatch(imageDescriptor, frameDescriptors, matches, 5);
+        ArrayList<DMatch> prunedMatches = pruneMatches(matches);
+
+        //go through the matches get use the indexes to get the relevant key points
+        List<KeyPoint> sceneKeyPoints = frameKeyPoints.toList();
+
+        int pixelBuf = 10;
+        byte[] green = new byte[]{(byte)0, (byte)255, (byte)0};
+
+        for (DMatch match : prunedMatches) {
+            Point pt = sceneKeyPoints.get(match.trainIdx).pt;
+            System.out.println(pt);
+
+            int midX = (int) pt.x;
+            int midY = (int) pt.y;
+            int minX = Math.max(0, midX - pixelBuf);
+            int maxX = Math.min(frame.width(), midX + pixelBuf);
+            int minY = Math.max(0, midY - pixelBuf);
+            int maxY = Math.min(frame.height(), midY + pixelBuf);
+
+            for (int x = minX; x < maxX; x++) {
+                for (int y = minY; y < maxY; y++) {
+                    frame.put(y,x,green);
+                }
+            }
+
+
+        }
+
+        System.out.println(frame.cols());
+        System.out.println(frame.rows());
+
+        Imgcodecs.imwrite("C:\\Users\\luke\\Desktop\\matches.png", frame);
+
         System.out.println("Found matches");
     }
 
-    private void pruneMatches(List<MatOfDMatch> matches){
-        matches.stream().filter(matOfDMatch -> {
-            DMatch[] dmatcharray = matOfDMatch.toArray();
+    private ArrayList<DMatch> pruneMatches(List<MatOfDMatch> matches){
+        ArrayList<DMatch> dMatches = new ArrayList<>();
+
+        for (MatOfDMatch match : matches) {
+            DMatch[] dmatcharray = match.toArray();
             DMatch m1 = dmatcharray[0];
             DMatch m2 = dmatcharray[1];
-            return m1.distance <= m2.distance * nndrRatio;
-        });
+            if(m1.distance <= m2.distance * nndrRatio){
+                dMatches.add(m1);
+            }
+        }
+        return dMatches;
     }
 
 
