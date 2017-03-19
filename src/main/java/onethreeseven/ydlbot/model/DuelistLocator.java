a1a8c7a91ddf1de0473c787a1bedd9d30fecd153
Luke Bermingham <luke.bermingham@2bitstudios.com.au>
2017-03-19 18:28:53 +1000

    Got correct results tweaking hough transform.

--- src/main/java/onethreeseven/ydlbot/model/DuelistLocator.java
+++ src/main/java/onethreeseven/ydlbot/model/DuelistLocator.java
@@ -1,15 +1,14 @@
 package onethreeseven.ydlbot.model;
 
-import onethreeseven.ydlbot.util.ImageUtil;
 import org.opencv.core.*;
-import org.opencv.features2d.DescriptorExtractor;
-import org.opencv.features2d.DescriptorMatcher;
-import org.opencv.features2d.FeatureDetector;
 import org.opencv.imgcodecs.Imgcodecs;
-import java.util.ArrayList;
-import java.util.List;
+import org.opencv.imgproc.Imgproc;
 
-import static org.opencv.core.CvType.CV_32F;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+import static org.opencv.imgcodecs.Imgcodecs.CV_LOAD_IMAGE_COLOR;
 
 
 /**
@@ -18,111 +17,137 @@ import static org.opencv.core.CvType.CV_32F;
  */
 public class DuelistLocator {
 
-//    /**
-//     * The detector we use for finding duelists.
-//     */
-//    private final CascadeClassifier detector =
-//            new CascadeClassifier(getClass().getResource("/lbpcascade_frontalface.xml").getPath());
-//
-//    public void findDuelist(){
-//        Mat frame = ImageUtil.getOpenCVScreenShot();
-//        // Detect faces in the image.
-//        MatOfRect detections = new MatOfRect();
-//        detector.detectMultiScale(frame, detections);
-//        System.out.println(String.format("Detected %s faces", detections.toArray().length));
-//    }
+    private static final int colR = 204;
+    private static final int colG = 170;
+    private static final int colB = 145;
+    private static final int rangeR = 51;
+    private static final int rangeG = 68;
+    private static final int rangeB = 60;
+    private static final int minR = colR - rangeR;
+    private static final int maxR = colR + rangeR;
+    private static final int minG = colG - rangeG;
+    private static final int maxG = colG + rangeG;
+    private static final int minB = colB - rangeB;
+    private static final int maxB = colB + rangeB;
+    private static final double skinPercentageThreshold = 0.1;
 
-    private static final FeatureDetector featureDetector = FeatureDetector.create(FeatureDetector.ORB);
-    private static final DescriptorExtractor descriptorExtractor = DescriptorExtractor.create(DescriptorExtractor.ORB);
-    private static final DescriptorMatcher descriptorMatcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE);
-    private static final float nndrRatio = 0.85f;
+    private static final Scalar debugColor = new Scalar(255, 0, 0);
 
-    private final Mat imageToLookFor;
-    private final Mat imageDescriptor;
-    private final MatOfKeyPoint imageKeyPoints;
-
-    //private final Mat duelistDescriptor;
+    private final String testFilePath;
+    private final String maskFilePath;
 
     public DuelistLocator(){
-        //String imgPath = Thread.currentThread().getContextClassLoader().getResource("save.png").getPath();
-        String imgPath = "C:\\Users\\luke\\Desktop\\projects\\ydl-bot\\src\\main\\resources\\key3.png";
-        imageToLookFor = Imgcodecs.imread(imgPath, Imgcodecs.IMREAD_COLOR);
-        imageKeyPoints = getKeyPoints(imageToLookFor);
-        imageDescriptor = getDescriptors(imageToLookFor, imageKeyPoints);
-    }
-
-    private MatOfKeyPoint getKeyPoints(Mat image){
-        MatOfKeyPoint objectKeyPoints = new MatOfKeyPoint();
-        featureDetector.detect(image, objectKeyPoints);
-        return objectKeyPoints;
-    }
-
-    private Mat getDescriptors(Mat image, MatOfKeyPoint objectKeyPoints){
-        Mat descriptor = new Mat();
-        descriptorExtractor.compute(image, objectKeyPoints, descriptor);
-        return descriptor;
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        String path1 = "";
+        String path2 = "";
+        try {
+            path1 = new File(loader.getResource("test_1.jpg").toURI()).getAbsolutePath();
+            path2 = new File(loader.getResource("mask.png").toURI()).getAbsolutePath();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        testFilePath = path1;
+        maskFilePath = path2;
     }
 
     public void findDuelist(){
-        //get the duel links scene and descriptors
-        Mat frame = ImageUtil.getOpenCVScreenShot();
-        MatOfKeyPoint frameKeyPoints = getKeyPoints(frame);
-        Mat frameDescriptors = getDescriptors(frame, frameKeyPoints);
-        //find matches of the duelist image in the scene
-        List<MatOfDMatch> matches = new ArrayList<>();
+//        //get the frame and convert it black and white threshold
+//        Mat frame = ImageUtil.getOpenCVScreenShot();
+//        Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BGR2GRAY);
 
-        imageDescriptor.convertTo(imageDescriptor, CV_32F);
-        frameDescriptors.convertTo(frameDescriptors, CV_32F);
+        //get the scene image
 
-        descriptorMatcher.knnMatch(imageDescriptor, frameDescriptors, matches, 5);
-        ArrayList<DMatch> prunedMatches = pruneMatches(matches);
 
-        //go through the matches get use the indexes to get the relevant key points
-        List<KeyPoint> sceneKeyPoints = frameKeyPoints.toList();
+        //"C:\\Users\\luke\\Desktop\\test_1.jpg";
+        //"C:\\Users\\luke\\Desktop\\mask.png";
+        //keep the raw image (in color) because we do skin matching on it
+        Mat raw = Imgcodecs.imread(testFilePath, CV_LOAD_IMAGE_COLOR);
+        //mask out most of the screen, only want duelist area
+        Mat mask = Imgcodecs.imread(maskFilePath, CV_LOAD_IMAGE_COLOR);
+        Size dims = new Size(raw.cols(), raw.rows());
+        Mat masked = new Mat(dims, CvType.CV_8UC3);
+        raw.copyTo(masked, mask);
+        //don't need raw anymore
+        raw.release();
+        //convert a copy of it to gray-scale for circle detection
+        Mat scene = new Mat(dims, CvType.CV_8UC1);
+        Imgproc.cvtColor(masked, scene, Imgproc.COLOR_BGR2GRAY);
+        //run it through a strong threshold to remove nearly everything except bright objects
+        Imgproc.threshold(scene, scene, 225, 255, Imgproc.THRESH_BINARY);
 
-        int pixelBuf = 10;
-        byte[] green = new byte[]{(byte)0, (byte)255, (byte)0};
+        //find circles
+        Mat circles = new Mat();
+        Imgproc.HoughCircles(scene, circles,
+                Imgproc.CV_HOUGH_GRADIENT,
+                1, 30, 250, 20, 10, 70);
 
-        for (DMatch match : prunedMatches) {
-            Point pt = sceneKeyPoints.get(match.trainIdx).pt;
-            System.out.println(pt);
+        //turn on if you want to see debug circles
+        //drawDebugCircles(scene, circles);
+        //go through each circle we found and decide whether to keep it or not
+        ArrayList<double[]> goodCircles = pruneCircles(masked, circles);
+        for (double[] goodCircle : goodCircles) {
+            drawDebugCircle(masked, goodCircle);
+        }
+        // Save the visualized detection.
+        File debugOutputFile = new File("debug_duelists.png");
+        Imgcodecs.imwrite(debugOutputFile.getAbsolutePath(), masked);
+        System.out.println("Collect debug file at: " + debugOutputFile.getAbsolutePath());
+    }
 
-            int midX = (int) pt.x;
-            int midY = (int) pt.y;
-            int minX = Math.max(0, midX - pixelBuf);
-            int maxX = Math.min(frame.width(), midX + pixelBuf);
-            int minY = Math.max(0, midY - pixelBuf);
-            int maxY = Math.min(frame.height(), midY + pixelBuf);
+    private void drawDebugCircle(Mat image, double[] circle){
+        double x = circle[0];
+        double y = circle[1];
+        int radius = (int)circle[2];
+        Imgproc.circle(image, new Point(x,y), radius, debugColor, 1);
+    }
 
-            for (int x = minX; x < maxX; x++) {
-                for (int y = minY; y < maxY; y++) {
-                    frame.put(y,x,green);
+    private ArrayList<double[]> pruneCircles(Mat raw, Mat circles){
+        final int width = raw.cols();
+        final int height = raw.rows();
+        final byte[] bgrColor = new byte[3];
+
+        //store circles in array {x,y,radius}
+        ArrayList<double[]> goodCircles = new ArrayList<>();
+
+        for(int i = 0; i < circles.cols(); i++) {
+            //x,y,radius
+            double[] circle = circles.get(0, i);
+            int centerX = (int) circle[0];
+            int centerY = (int) circle[1];
+            int radius = (int)circle[2];
+
+            //go through pixels and check if the threshold for skin colored pixels is met
+            int skinColoredPixelsFound = 0;
+            int totalPixelsExamined = 0;
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    int offsetX = x - centerX;
+                    int offsetY = y - centerY;
+                    //only do circle indices
+                    if (offsetX*offsetX + offsetY*offsetY <= radius*radius) {
+                        raw.get(y,x,bgrColor);
+                        int b = bgrColor[0] + 128;
+                        int g = bgrColor[1] + 128;
+                        int r = bgrColor[2] + 128;
+
+
+
+                        if(r <= maxR && r >= minR &&
+                                g <= maxG && g >= minG &&
+                                b <= maxB && b >= minB){
+                            skinColoredPixelsFound++;
+                        }
+                        totalPixelsExamined++;
+                    }
                 }
             }
 
-
-        }
-
-        System.out.println(frame.cols());
-        System.out.println(frame.rows());
-
-        Imgcodecs.imwrite("C:\\Users\\luke\\Desktop\\matches.png", frame);
-
-        System.out.println("Found matches");
-    }
-
-    private ArrayList<DMatch> pruneMatches(List<MatOfDMatch> matches){
-        ArrayList<DMatch> dMatches = new ArrayList<>();
-
-        for (MatOfDMatch match : matches) {
-            DMatch[] dmatcharray = match.toArray();
-            DMatch m1 = dmatcharray[0];
-            DMatch m2 = dmatcharray[1];
-            if(m1.distance <= m2.distance * nndrRatio){
-                dMatches.add(m1);
+            double percentageSkin = skinColoredPixelsFound/(double)totalPixelsExamined;
+            if(percentageSkin >= skinPercentageThreshold){
+                goodCircles.add(circle);
             }
         }
-        return dMatches;
+        return goodCircles;
     }
 
 
